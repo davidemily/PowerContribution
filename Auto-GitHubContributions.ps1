f3c5d776008568d71a25d34786e86bbccec9d0ac
davidemily <david.emily@live.com>
2019-05-26 19:34:55 -0500

    script creating random file and pushing to master

--- Auto-GitHubContributions.ps1
+++ Auto-GitHubContributions.ps1
@@ -1,1 +1,61 @@
-todo:
+Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Repository#,
+
+    # [Parameter(Mandatory=$True)]
+    # [ValidateNotNullOrEmpty()]
+    # [string]$Username,
+
+    # [Parameter(Mandatory=$true)]
+    # [ValidateNotNullOrEmpty()]
+    # [SecureString]$Password
+)
+
+function get-randomString {
+    $result = 1..10 |
+    ForEach-Object{
+        $generator = Get-Random -Minimum 97 -Maximum 123
+        [char][byte]$generator
+    }
+    $result -join ''
+}
+
+$folderName = get-randomString
+Write-Host "Creating folder $folderName.."
+mkdir $folderName
+Start-Sleep -s 3
+cd $folderName
+git init
+Start-Sleep -s 3
+Write-Host "Attempting to pull repository.."
+try {
+    git pull $Repository
+    Start-Sleep -s 3
+    Write-Host "Successfully pulled repository"
+    $fileName = get-randomString
+    Write-Host "About to create file $fileName.."
+    echo "Made by AutoContribute" >> $fileName
+    Write-Host "$fileName created"
+    Start-Sleep 3
+    git add .
+    git commit -m "created $fileName.md"
+    Start-Sleep 3
+    git push --set-upstream $Repository master
+    Write-Host "Pushed to repository successfully"
+    Start-Sleep 3
+}
+catch {
+    Write-Host "An error occurred:"
+    Write-Host $_
+    Write-Host "Please check repository exists and you are connected to the internet"
+}
+finally{
+    Write-Host "Deleting local folder"
+    cd ..
+    Write-Host "Removing folder $folderName.."
+    Start-Sleep 3
+    rmdir -Force -Recurse $folderName
+    Write-Host "All clean!"
+}
+
